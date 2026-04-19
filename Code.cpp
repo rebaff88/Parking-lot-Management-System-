d2ca0ac72b85c20ce12ea990d08163af1c80f4db
RebaFawad <rebaff88@gmail.com>
2026-04-19 20:52:43 +0500

    Delete Code.cpp

--- Code.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main() {
-	//write your code part below; Also try to 
-	//use the concepts of OOp we have mentioned in the proposal , also
-	//do use header files in your code 
-	cout << "Hello World" << endl;
-	return 0;
-}
