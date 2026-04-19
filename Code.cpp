62539c3fd0ab1800ee2d5360cf99744b5bf85eb3
RebaFawad <rebaff88@gmail.com>
2026-04-19 21:32:01 +0500

    Add files via upload

---
+++ Code.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main() {
+	//write your code part below; Also try to 
+	//use the concepts of OOp we have mentioned in the proposal , also
+	//do use header files in your code 
+	return 0;
+}
